b1f5b7c88fdf42f91146b7541680455cd085de74
herreio <mail@herre.io>
2019-09-30 01:16:33 +0200

    src/utils/ updated

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -1,8 +1,9 @@
 import os
 
 FILE_DIR, _ = os.path.split(os.path.abspath(__file__))
-
-MAIN_DIR = os.path.join(FILE_DIR, '../../../')
+PROJECT_DIR = os.path.join(FILE_DIR, '../../')
+MAIN_DIR = os.path.join(PROJECT_DIR, '../')
+LOG_DIR = os.path.join(PROJECT_DIR, 'log/')
 
 BASE_DIR = os.path.join(MAIN_DIR, 'base/data/')
 MPIS_DIR = os.path.join(BASE_DIR, 'mpis/')
@@ -12,3 +13,6 @@ ITEMS_DIR = os.path.join(BASE_DIR, 'items/')
 DATA_DIR = os.path.join(MAIN_DIR, 'data/')
 STATS_OUT = os.path.join(DATA_DIR, 'count/')
 TITLES_OUT = os.path.join(DATA_DIR, 'titles/')
+
+GRAPH_DIR = os.path.join(DATA_DIR, 'graph/')
+TABLES_DIR = os.path.join(DATA_DIR, 'tables/')
