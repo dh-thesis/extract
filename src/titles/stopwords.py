b4515c114605a08497f22f790c363e8f29443016
herreio <mail@herre.io>
2019-10-19 20:43:20 +0200

    src/titles/stopwords.py clean up

--- src/titles/stopwords.py
+++ src/titles/stopwords.py
@@ -3,19 +3,24 @@ import os
 dirname, filename = os.path.split(os.path.abspath(__file__))
 resources = os.path.join(dirname, 'resources/')
 
+
 def read_wordlist(fpath):
     with open(fpath, 'r') as fp:
         return [line.strip() for line in fp.readlines()]
+
 
 def get_wordslist(fname):
     path = os.path.join(resources, fname)
     return read_wordlist(path)
 
+
 def get_smart(fname='smart--stopwords-en.txt'):
     return get_wordslist(fname)
 
+
 def get_edit(fname='editorial--stopwords-en.txt'):
     return get_wordslist(fname)
+
 
 def get_stopwords(words='smart'):
     if words == 'smart':
