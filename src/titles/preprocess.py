604288da8132da8be85de8aee272a31131d33dab
herreio <mail@herre.io>
2019-10-19 18:06:16 +0200

    src/titles/preprocess.py clean up

--- src/titles/preprocess.py
+++ src/titles/preprocess.py
@@ -10,10 +10,11 @@ stop = get_stopwords('smart')
 punct = re.compile('[%s—]' % re.escape(string.punctuation))
 chars = re.compile('[^%s]' % re.escape(string.printable))
 
+
 def clean(title):
     title = clean_title(title)
     title = title.lower()
-    title = punct.sub(" ",title).strip()
+    title = punct.sub(" ", title).strip()
     title = remove_numbers(title)
     title = remove_stopwords(title)
     title = remove_nonascii(title)
@@ -22,17 +23,22 @@ def clean(title):
     title = title.strip()
     return title
 
+
 def remove_stopwords(title):
     return " ".join([word for word in title.split() if word not in stop])
+
 
 def remove_short(title):
     return " ".join([word for word in title.split() if len(word) > 2])
 
+
 def remove_nonascii(title):
-    return " ".join([word for word in title.split() if word == chars.sub(" ",word)])
+    return " ".join([word for word in title.split() if word == chars.sub(" ", word)])
+
 
 def remove_numbers(title):
     return " ".join([word for word in title.split() if not has_number(word)])
+
 
 def has_number(word):
     return any(i.isdigit() for i in word)
