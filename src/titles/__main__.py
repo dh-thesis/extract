18aa2d23b31e2ad02bfc57097abf2dd1844aafa5
herreio <mail@herre.io>
2019-10-21 20:30:00 +0200

    src/titles/ updated

--- src/titles/__main__.py
+++ src/titles/__main__.py
@@ -4,9 +4,12 @@ import time
 
 from pybman import utils
 
+from . import compile
+from . import docterm
+
 from ..utils.paths import LOG_DIR
 
-print("start extraction of titles!")
+print("start title preparation!")
 
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
@@ -27,50 +30,57 @@ print("")
 dirname, _ = os.path.split(os.path.abspath(__file__))
 fpath = dirname + "/resources/genres.txt"
 genres = utils.read_plain_clean(fpath)
-from .compile import *
 print("")
 print("##############")
 print("## LANGUAGE ##")
 print("##############")
 print("")
 print("extracting titles with shared language...")
-titles_from_lang()
+compile.titles_from_lang()
 print("extracting titles with shared language and year...")
-titles_from_lang_by_year()
+compile.titles_from_lang_by_year()
 print("extracting titles with shared language, year and genre...")
 for g in genres:
     print("about to extract titles from records of type", g,"...")
-    titles_from_lang_in_genre_by_year(genre=g)
+    compile.titles_from_lang_in_genre_by_year(genre=g)
 print("")
 print("###############")
 print("## INSTITUTE ##")
 print("###############")
 print("")
 print("extracting titles of institutes with shared language...")
-titles_from_mpis()
+compile.titles_from_mpis()
 print("extracting titles of institutes with shared language and year...")
-titles_from_mpis_by_year()
+compile.titles_from_mpis_by_year()
 print("extracting titles of institutes with shared language, year and genre...")
 for g in genres:
-    titles_from_mpis_in_genre_by_year(genre=g)
+    compile.titles_from_mpis_in_genre_by_year(genre=g)
 print("")
 print("##############")
 print("## CATEGORY ##")
 print("##############")
 print("")
 print("extracting titles of categories with shared language...")
-titles_from_cats()
+compile.titles_from_cats()
 print("extracting titles of categories with shared language and year...")
-titles_from_cats_by_year()
+compile.titles_from_cats_by_year()
 print("extracting titles of categories with shared language, year and genre...")
 for g in genres:
-    titles_from_cats_in_genre_by_year(genre=g)
+    compile.titles_from_cats_in_genre_by_year(genre=g)
 done = time.time()
 print("----------------------------------------")
 print("finished extraction on", time.ctime(done))
 print("after", round((done - start) / 60, 2), "minutes!")
-
+start = time.time()
+print("----------------------------------------")
+print("start building documents on", time.ctime(start))
+docterm.routine()
+done = time.time()
+print("finished building documents on", time.ctime(done))
+print("after", round((done - start) / 60, 2), "minutes!")
+start = time.time()
+print("----------------------------------------")
 log.close()
 sys.stdout = stdout
 
-print("finished extraction after %s min!" % round((done - start)/60, 2))
+print("finished after %s min!" % round((done - start)/60, 2))
